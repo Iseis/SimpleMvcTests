a364df77bb866b6f9aef5dafc4a6d0f1b358eb17
U-STATES\aowens <aowens@sts0738.states.win.statesind.com>
2016-07-19 15:19:55 -0700

    Second push

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Titile { get; set; }
+        public string Genre { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
